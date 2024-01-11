e8986fbb1c1e30f9c343e4ae23e7d25c0432cd08
sparky-netizen <asolp.09@gmail.com>
2024-01-10 20:07:18 -0600

    actualizacion

--- custom-components.js
+++ custom-components.js
@@ -27,6 +27,7 @@ class CustomNavbar extends HTMLElement {
   }
 }
 
+
 class StreamingCard extends HTMLElement {
   static get observedAttributes() {
     return ['imgSrc', 'text', 'link'];
@@ -76,5 +77,56 @@ class StreamingCard extends HTMLElement {
   }
 }
 
+
+
+class StreamingcardCombo extends HTMLElement  {
+  static get observedAttributes() {
+    return['imgSrc', 'text', 'link']
+  }
+
+constructor() {
+  super();
+  this.attachShadow({ mode: 'open' });
+}
+
+async connectedCallback() {
+  const shadow = this.shadowRoot;
+  shadow.innerHTML = await this.getTemplate();
+  /** @type {HTMLImageElement}*/
+  this.img = shadow.getElementById('cardcombo-image');
+  /** @type {HTMLElement} */
+  this.text = shadow.getElementById('cardcombo-text');
+  /** @type {HTMLAnchorElement} */
+  this.link = shadow.getElementById('cardcombo-link');
+
+  this.img.src = this.getAttribute('imgSrc');
+    this.text.innerText = this.getAttribute('text');
+    this.link.href = this.getAttribute('link');
+}
+  
+attributeChangedCallback(name, oldValue, newValue) {
+  switch (name) {
+    case 'imgSrc':
+      if (this.img) this.img.src = newValue;
+      break
+      case 'text':
+        if (this.text) this.text.innerText = newValue;
+        break
+        case 'link':
+          if (this.link) this.link.href = newValue;
+          break;
+          default:
+          break;
+  }
+}
+async getTemplate() {
+  const htmlFile = await fetch('../components/streaming-cardcombo.html');
+  const template = await htmlFile.text();
+  return template;
+}
+}
+
 customElements.define('custom-navbar', CustomNavbar);
 customElements.define('streaming-card', StreamingCard);
+customElements.define('streaming-cardcombo', StreamingcardCombo);
+
